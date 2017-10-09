a8b1259b5578255e2fd10a1f8393878e35b17d70
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2017-10-09 15:46:02 +0530

    Initial check-in

---
+++ AppSettingsApp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppSettingsApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			if (IsFeatureEnabled())
+			{
+				throw new InvalidOperationException();
+			}
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			if (IsFeatureEnabled())
+			{
+				throw new InvalidOperationException();
+			}
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			if (IsFeatureEnabled())
+			{
+				throw new InvalidOperationException();
+			}
+
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		private bool IsFeatureEnabled()
+		{
+			bool featureEnabledStatus = true;
+			const string featureStatuskey = "EnableNewFeature";
+
+			try
+			{
+				var appSettings = ConfigurationManager.AppSettings;
+				featureEnabledStatus = Convert.ToBoolean(appSettings[featureStatuskey]);
+			}
+			catch (ConfigurationErrorsException)
+			{
+				Console.WriteLine("Error reading app settings");
+			}
+
+
+			return featureEnabledStatus;
+		}
+	}
+}
